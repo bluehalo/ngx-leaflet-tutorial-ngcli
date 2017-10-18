adde1a9abb6ab4586ddda441ea1eeb6af40c4e03
Ryan Blace <reblace@gmail.com>
2017-10-17 23:13:39 -0400

    Creating a basic fullscreen map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { latLng, tileLayer } from 'leaflet';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  googleMaps = tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+    detectRetina: true
+  });
+
+  layers = [ this.googleMaps ];
+  options = { zoom: 7, center: latLng([ 46.879966, -121.726909 ]) };
+
 }
